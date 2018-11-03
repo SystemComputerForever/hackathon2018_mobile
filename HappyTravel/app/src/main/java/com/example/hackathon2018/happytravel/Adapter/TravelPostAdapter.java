34f6c7e354fef9f41a02ebcfb847d8bcc444c607
SystemComputer <computersystemforever@gmail.com>
2018-11-04 02:29:20 +0800

    titile change

--- HappyTravel/app/src/main/java/com/example/hackathon2018/happytravel/Adapter/TravelPostAdapter.java
+++ HappyTravel/app/src/main/java/com/example/hackathon2018/happytravel/Adapter/TravelPostAdapter.java
@@ -4,49 +4,55 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.example.hackathon2018.happytravel.R;
+
 import java.util.ArrayList;
 
 /**
  * Created by Benix on 3/11/2018.
  */
 
-public class TravelPostAdapter extends RecyclerView.Adapter<TravelPostAdapter.ViewHolder>
-{
+public class TravelPostAdapter extends RecyclerView.Adapter<TravelPostAdapter.ViewHolder> {
     private ArrayList<String> data;
 
-    public TravelPostAdapter(ArrayList<String> data)
-    {
+    public TravelPostAdapter(ArrayList<String> data) {
         this.data = data;
     }
 
     @Override
-    public TravelPostAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-    {
+    public TravelPostAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_post, parent, false);
         ViewHolder viewholder = new ViewHolder(view);
         return viewholder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position)
-    {
-      //  holder.txtItem.setText(mDataSet.get(position));
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText("a");
     }
 
     @Override
-    public int getItemCount()
-    {
+    public int getItemCount() {
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder
-    {
-       // public TextView txtItem;
-        public ViewHolder(View v)
-        {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // public TextView txtItem;
+        private ImageView image;
+        private TextView title;
+        private TextView startdate;
+        public TextView userid;
+
+
+        public ViewHolder(View v) {
             super(v);
-           // txtItem = (TextView) v.findViewById(R.id.txtItem);
+            image = v.findViewById(R.id.imageView);
+            title = v.findViewById(R.id.textView);
+            startdate = v.findViewById(R.id.startdate);
+            userid = v.findViewById(R.id.userid);
         }
     }
 }
--- HappyTravel/app/src/main/java/com/example/hackathon2018/happytravel/UIActiviy/TravelpostActivity.java
+++ HappyTravel/app/src/main/java/com/example/hackathon2018/happytravel/UIActiviy/TravelpostActivity.java
@@ -2,11 +2,17 @@ package com.example.hackathon2018.happytravel.UIActiviy;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.hackathon2018.happytravel.Adapter.TravelPostAdapter;
 import com.example.hackathon2018.happytravel.Controller.RestController;
 import com.example.hackathon2018.happytravel.Function.CallBackFunction;
 import com.example.hackathon2018.happytravel.R;
 import com.example.hackathon2018.happytravel.UIActiviy.BasicActivity.NavigationActivity;
+
+import java.util.ArrayList;
 
 
 public class TravelpostActivity extends NavigationActivity implements CallBackFunction {
@@ -19,25 +25,21 @@ public class TravelpostActivity extends NavigationActivity implements CallBackFu
         setContentView(R.layout.activity_travelpost);
         super.onCreate(savedInstanceState);
         mContext = this;
-       /* RecyclerView recyclerView;
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView;
         ArrayList<String> Dataset = new ArrayList<String>();
         for(int i = 0; i < 10; i++)
         {
             Dataset.add(i + "");
         }
-        MyAdapter myAdapter = new MyAdapter(myDataset);
+        TravelPostAdapter myAdapter = new TravelPostAdapter(Dataset);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this); //設定此 layoutManager 為 linearlayout (類似ListView)
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL); 設定此 layoutManager 為垂直堆疊
-
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
                 recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST)); //設定分割線
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL)); //設定分割線
         recyclerView.setLayoutManager(layoutManager); //設定 LayoutManager
         recyclerView.setAdapter(myAdapter); //設定 Adapter*/
         refresh();
     }
-
     public void refresh() {
         //tpc = new RestController(progress_form, mContext,  drawer);
         //tpc.execute();
